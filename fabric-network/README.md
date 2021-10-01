24522e1e00709df4069b7ec54f71e2ba2cd50f5b
D <d_kelsey@uk.ibm.com>
2021-10-01 14:24:43 +0100

    Update network to use latest images

--- fabric-network/README.md
+++ fabric-network/README.md
@@ -1,1 +1,3 @@
 # Build a network
+
+dowload te 2.3.x binaries for fabric and unpack the tar file ensuring the binaries unpack into the bin directory and the config files unpack into the config directory
--- fabric-network/docker-based-syschannel/network.sh
+++ fabric-network/docker-based-syschannel/network.sh
@@ -18,6 +18,10 @@ export VERBOSE=false
 
 . scripts/utils.sh
 
+infoln "pulling latest peer and orderer images"
+docker pull hyperledger-fabric.jfrog.io/fabric-orderer:amd64-latest
+docker pull hyperledger-fabric.jfrog.io/fabric-peer:amd64-latest
+
 # Obtain CONTAINER_IDS and remove them
 # TODO Might want to make this optional - could clear other containers
 # This function is called when you bring a network down
