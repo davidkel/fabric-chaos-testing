0ee015604cfb0815a51b3a5839bf8e55491a1e8a
sapthasurendran <saptha.surendran@ibm.com>
2021-10-08 02:06:17 +0530

    Lint and config file  updates
    Signed-off-by: sapthasurendran <saptha.surendran@ibm.com>

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,66 +1,57 @@
 import * as env from 'env-var';
 
 export const logLevel = env
-  .get('LOG_LEVEL')
-  .default('info')
-  .asEnum([ 'error', 'warn', 'info', 'debug']);
+    .get('LOG_LEVEL')
+    .default('info')
+    .asEnum([ 'error', 'warn', 'info', 'debug']);
 
 export const ORGS = env
-  .get('ORGS')
-  .required()
-  .asJson()
+    .get('ORGS')
+    .required()
+    .asJson()
 
 export const ORG = env
-  .get('ORG')
-  .required()
-  .default('Org1MSP')
-  .asString()
+    .get('ORG')
+    .required()
+    .default('Org1MSP')
+    .asString()
 
 
-  export const TRANSACTION_COUNT = env
-  .get('TRANSACTION_COUNT')
-  .required()
-  .default(20)
-  .asIntPositive()
+export const TRANSACTION_COUNT = env
+    .get('TRANSACTION_COUNT')
+    .required()
+    .default(20)
+    .asIntPositive()
 
-  export const CHAINCODE_DATA = env
-  .get('CHAINCODE_DATA')
-  .default({"FUNCTION":"createChaosAsset",
-  "ARGS":[`asset${Math.random}`,'value']})
-  .required()
-  .asJson()
+export const CHAINCODE_DATA = env
+    .get('CHAINCODE_DATA')
+    .required()
+    .asJson()
 
+export const peerEndPoint =env
+    .get('PEER_ENDPOINT')
+    .required()
+    .default('')
+    .asString()
 
-
-
-
-  export const peerEndPoint =env
-  .get('PEER_ENDPOINT')
-  .required()
-  .default('')
-  .asString()
-
-  export const channelName = env
-  .get('CHANNEL_NAME')
-  .required()
-  .default('mychannel')
-  .asString();
+export const channelName = env
+    .get('CHANNEL_NAME')
+    .required()
+    .default('mychannel')
+    .asString();
 
 export const chaincodeName = env
-  .get('CHAINCODE_NAME')
-  .required()
-  .default('basic')
-  .asString();
+    .get('CHAINCODE_NAME')
+    .required()
+    .default('basic')
+    .asString();
 
 export const batchInterval = env.get('BATCH_INTERVAL').default(2000).asIntPositive();
 
 
-
-
-
 export const gatewayPeer = env
-  .get('GATEWAY_PEER')
-  .asString();
+    .get('GATEWAY_PEER')
+    .asString();
 
 
 
