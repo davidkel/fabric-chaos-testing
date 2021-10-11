b34e8ccbccf8e0568bb821bbc644787ff33ee483
sapthasurendran <saptha.surendran@ibm.com>
2021-10-11 20:58:59 +0530

    removed singleton class for contract
    Signed-off-by: sapthasurendran <saptha.surendran@ibm.com>

--- client/src/contract.ts
+++ client/src/contract.ts
@@ -3,72 +3,57 @@ import { logger } from './utils/logger';
 import { Contract, Gateway, Network, ProposalOptions } from 'fabric-gateway';
 
 export class CCHelper {
-  private static contractInstance: CCHelper;
-  private static contract: Contract;
-  private static network: Network;
+  contract: Contract;
+  network: Network;
   channel = '';
   chaincode = '';
 
-  private constructor(gateway: Gateway, channel: string, chaincode: string) {
-      CCHelper.network = gateway.getNetwork(channel);
+  constructor(gateway: Gateway, channel: string, chaincode: string) {
+      this.network = gateway.getNetwork(channel);
       logger.info(
           'Created a network instance based on channel %s for user ',
           channel
       );
-      CCHelper.contract = CCHelper.network.getContract(chaincode);
-      return CCHelper.contractInstance;
+      this.contract = this.network.getContract(chaincode);
   }
 
-  static getInstance(
-      gateway: Gateway,
-      channel: string,
-      chaincode: string
-  ): CCHelper {
-      return (
-          CCHelper.contractInstance ||
-      (CCHelper.contractInstance = new CCHelper(gateway, channel, chaincode))
-      );
+  getContract(): Contract {
+      return this.contract;
   }
-
-  getContractInstance(): Contract {
-      return CCHelper.contract;
-  }
-
-
 
   getNetwork(): Network {
-      return CCHelper.network;
+      return this.network;
   }
 
   async submitTransaction(func: string, args: string[]): Promise<void> {
       try {
-          logger.info(
-              'To invoke chaincode %s ',
-              CCHelper.contract.getChaincodeId()
-          );
+          logger.info('To invoke chaincode %s ', this.contract.getChaincodeId());
           const opts: ProposalOptions = {
               arguments: args,
           };
-          const proposal = CCHelper.contract.newProposal(func, opts);
+          const proposal = this.contract.newProposal(func, opts);
           logger.info('Created proposal for transaction CreatecreateChaosAsset');
+          const txnID = proposal.getTransactionId();
 
           logger.info('About to endorse transaction ');
           const txn = await proposal.endorse();
-          const txnID = txn.getTransactionId();
           logger.info('Endorsement successful for transactionID: %s', txnID);
+
           logger.info(`Received endorsed, broadcast to orderer for txnid ${txnID}`);
           const subtx = await txn.submit();
           logger.info(`Transaction submitted for txnID: ${txnID}`);
 
           const stat = await subtx.getStatus();
           logger.info(`Transaction status for txnID: ${txnID} status:%s`, stat);
+
+
       } catch (e) {
           logger.error('Error invoking chaincode', e);
       }
   }
 
   async evaluateTransaction(func: string, args: string[]): Promise<void> {
-      const result = await CCHelper.contract.evaluateTransaction(func, ...args);
+      const result = await this.contract.evaluateTransaction(func, ...args);
       logger.info(Buffer.from(result).toString());
       logger.info('Received asset %s', Buffer.from(result).toString());
   }
