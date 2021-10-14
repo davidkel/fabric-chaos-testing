67a240d2f430e661a7702bcbf7a3d82ef94ba81a
sapthasurendran <saptha.surendran@ibm.com>
2021-10-14 13:12:45 +0530

    App transaction rate changes
    Signed-off-by: sapthasurendran <saptha.surendran@ibm.com>

--- client/src/app.ts
+++ client/src/app.ts
@@ -5,6 +5,7 @@ import { CCHelper } from './contract';
 
 import { GatewayHelper,OrgProfile } from './gateway';
 import { TransactionData } from './transactionData';
+import { sleep } from './utils/helper';
 
 
 interface Orgs {
@@ -12,72 +13,35 @@ interface Orgs {
 }
 
 
-interface ChaincodeData {
-  function: string;
-  args: string[];
-}
+
 class App {
-
+    keepRunning = true
     gateway!: Gateway;
-    rate = config.batchInterval
 
-    resetBatchInterval(min:number,max:number){
-        this.rate = Math.floor(Math.random() * (max - min + 1) + min)*1000;
-    }
+
 
     async main(): Promise<void> {
 
         this.gateway = await new GatewayHelper((config.ORGS as Orgs)[config.ORG]).configureGateway();
 
         const ccHelper = new CCHelper(this.gateway,config.channelName,config.chaincodeName);
-        const startExecution = (ccHelper:CCHelper) => {
-            this.execute(ccHelper);
-            this.resetBatchInterval(50,30);
-            setInterval(startExecution, this.rate, ccHelper);
-        }
-        startExecution(ccHelper)
-    }
+        const transactionData :TransactionData = new TransactionData();
 
+        while(this.keepRunning){
+            if( ccHelper.getUnfinishedTransactions() < config.MAX_UNFINISHED_TRANSACTION_COUNT){
+                ccHelper.runTransaction(transactionData.getTransactionDetails());
+            }
+            else{
+                await sleep(config.maxLimit,config.minLimit)
+            }
 
-    private async execute(ccHelper:CCHelper): Promise<void> {
-
-        const chaincodeData = config.CHAINCODE_DATA as ChaincodeData;
-        const data = new TransactionData(1,[],'submit',chaincodeData.function)
-        switch(data.transactionType){
-        case 'submit':
-            this.loop(()=>{
-                ccHelper.submitTransaction(
-                    data.function,
-                    data.args)},data.count
-            );
-            break;
-        case 'evaluate':
-            this.loop(()=>{
-                ccHelper.evaluateTransaction(
-                    data.function,
-                    data.args)},data.count
-            );
-            break;
-
-        }
-    }
-    loop(callback:()=>void,count:number):void{
-        for (let i = 0; i <= count; i++) {
-            callback();
         }
 
     }
 
 }
 
-
-
 new App().main().catch((error) =>
     console.log('******** FAILED to run the application:', error)
 );
-
-
-
-
-
 
