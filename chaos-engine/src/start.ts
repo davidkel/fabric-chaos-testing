400e756b205f09f140dc739075fda66157aa3e0b
D <d_kelsey@uk.ibm.com>
2021-12-23 16:16:19 +0000

    Improve the build and run environments
    Makes it easier to integrate into fabric-test
    
    Signed-off-by: D <d_kelsey@uk.ibm.com>

--- chaos-engine/src/start.ts
+++ chaos-engine/src/start.ts
@@ -2,8 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import sourceMapSupport from 'source-map-support'
-sourceMapSupport.install()
+import 'source-map-support/register';
 
 import {ScenarioRunner} from './scenariorunner'
 
--- coord.sh
+++ coord.sh
@@ -1,15 +1,19 @@
-    # start the client
-    cd client
-    # npm run build
-    node -r source-map-support/register --require dotenv/config dist/app.js &
-    last_pid=$!
-    sleep 10
-    cd ../chaos-engine
-    node dist/start.js ./scenarios/stop-scenarios peer1-org1 random 5
-    cd ..
-    sleep 60
-    kill -SIGINT $last_pid
-    wait $last_pid
-    client_exit=$?
-    echo $client_exit
-    exit $client_exit
+#!/bin/bash
+
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+echo 'waiting for network to discover itself'
+sleep 30
+echo 'Starting Client'
+node client/dist/app.js &
+last_pid=$!
+sleep 10
+node chaos-engine/dist/start.js ./scenarios/stop-scenarios peer1-org1 random 5
+echo 'Chaos engine completed, waiting 60 secs before terminating client'
+sleep 60
+kill -s SIGINT $last_pid
+wait $last_pid
+client_exit=$?
+echo $client_exit
+exit $client_exit
